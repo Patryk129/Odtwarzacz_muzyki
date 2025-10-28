cbac28bb34669d891fea85a87a477cd785ee9b62
Patryk129 <pp38410@gmail.com>
2025-10-28 12:12:30 +0100

    update

---
+++ Odtwarzacz_muzyki/AccountPage.xaml.cs
@@ -0,0 +1,25 @@
+namespace Odtwarzacz_muzyki;
+
+public partial class AccountPage : ContentPage
+{
+	public AccountPage()
+	{
+		InitializeComponent();
+	}
+    private async void Home_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MainPage");
+    }
+    private async void Search_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//SearchPage");
+    }
+    private async void Files_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//FilesPage");
+    }
+    private async void Account_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//AccountPage");
+    }
+}
--- Odtwarzacz_muzyki/MainPage.xaml.cs
+++ Odtwarzacz_muzyki/MainPage.xaml.cs
@@ -8,5 +8,22 @@
         {
             InitializeComponent();
         }
+
+        private async void Home_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//MainPage");
+        }
+        private async void Search_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//SearchPage");
+        }
+        private async void Files_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//FilesPage");
+        }
+        private async void Account_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//AccountPage");
+        }
     }
 }
